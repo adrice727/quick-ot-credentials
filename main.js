9f1aaa5d10528fcd40f459a0139bdb58ab63557a
Aaron Rice <adrice727@gmail.com>
2017-10-31 17:36:36 -0700

    Get ready for packaging.

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 600,
+    icon: path.join(__dirname, 'assets/images/400x400.png')});
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -37,7 +40,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
